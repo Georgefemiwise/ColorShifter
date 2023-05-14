f04449fa2a2329607ae8021d9cc9f4d850e2539f
georgefemiwise <georgewise1010@gmail.com>
2023-05-14 12:43:44 +0000

    first commit

---
+++ src/cardData.js
@@ -0,0 +1,34 @@
+
+
+export default [
+	{
+		id: 1,
+		code: '',
+		backgroundColor: colorGenerator()
+	},
+	{
+		id: 2,
+		code: '',
+		backgroundColor: colorGenerator()
+	},
+	{
+		id: 3,
+		code: '',
+		backgroundColor: colorGenerator()
+	},
+	{
+		id: 4,
+		code: '',
+		backgroundColor: colorGenerator()
+	},
+	{
+		id: 5,
+		code: '',
+		backgroundColor: colorGenerator()
+	},
+	{
+		id: 6,
+		code: '',
+		backgroundColor: colorGenerator()
+	},
+];
