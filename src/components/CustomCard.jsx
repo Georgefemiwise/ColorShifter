b39c8b7d0dc7f96b72fa13d90da5332927b54c12
georgefemiwise <georgewise1010@gmail.com>
2023-05-21 03:14:46 +0000

    rev

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 export default function CustomCard({ id, children, toggle, backgroundColor }) {
 	function copyHexCode(props) {
 		navigator.clipboard.writeText(backgroundColor).then(() => {
@@ -29,8 +28,7 @@ export default function CustomCard({ id, children, toggle, backgroundColor }) {
 					id='copyButton'
 					onClick={() => copyHexCode(backgroundColor)}
 					class='rounded-bl-xl select-none rounded-sm  py-1 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray shadow-lg shadow-black-500/20 transition-all hover:shadow-md hover:opacity-100 hover:text-white hover:shadow-gray-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none pointer opacity-20 hover:bg-[#835DCD]'
-					type='button'
-					>
+					type='button'>
 					copy
 				</button>
 			</div>
